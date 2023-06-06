55507bf740c1370b2ecfa39487a7c542821d419a
unknown <edencope.dev@gmail.com>
2023-06-05 22:19:26 -0300

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>The Prince and His Palace</h1>
+      <a href='https://www.gofundme.com/f/help-me-as-i-clean-up-the-world?utm_medium=email&utm_source=product&utm_campaign=p_email%2B2300-co-team-welcome' target='_blank'>GoFundMe</a>
+    </>
+  )
+}
+
+export default App
